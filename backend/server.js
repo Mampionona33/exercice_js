f4ef03bd8c82e10ba8d9e5e153408273ab32b8d8
Mampionona33 <ramamps33@gmail.com>
2022-02-08 21:36:03 +0300

    problem server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
 	res.send('hello world');
 });
+
+app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(port, () => {
 	console.log(`server start on port ${port}`);
